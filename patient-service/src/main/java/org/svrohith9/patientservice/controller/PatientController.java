f76efee353ef260e66aa1b3555e208a58c838ca3
Rohith Sv <svrohith9@gmail.com>
2025-03-27 22:22:21 -0400

    Swagger integration and CORS Fix

--- patient-service/src/main/java/org/svrohith9/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/org/svrohith9/patientservice/controller/PatientController.java
@@ -1,5 +1,7 @@
 package org.svrohith9.patientservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/patients/v1")
+@CrossOrigin(origins = {"http://localhost:8080","https://editor.swagger.io"})
+@Tag(name = "Patient", description = "Patient Controller")
 public class PatientController {
 
     private final PatientService patientService;
@@ -20,22 +24,26 @@ public class PatientController {
     }
 
     @GetMapping
+    @Operation(summary = "Get Patients")
     public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok(patients);
     }
 
     @PostMapping
+    @Operation(summary = "Create Patient")
     public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
         return ResponseEntity.ok(patientService.createPatient(patientRequestDTO));
     }
 
     @PutMapping("/{uuid}")
+    @Operation(summary = "Update patient")
     public ResponseEntity<PatientResponseDTO> updatePatient(@Valid @PathVariable java.util.UUID uuid, @Valid @RequestBody PatientRequestDTO patientRequestDTO) {
         return ResponseEntity.ok(patientService.updatePatient(uuid, patientRequestDTO));
     }
 
     @DeleteMapping("/{uuid}")
+    @Operation(summary = "Delete patient")
     public ResponseEntity<Void> deletePatient(@Valid @PathVariable java.util.UUID uuid) {
         patientService.deletePatient(uuid);
         return ResponseEntity.noContent().build();
